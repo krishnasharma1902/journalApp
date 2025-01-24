ae5f9a7d225288fc26cb00b4682c61ce47cc8305
unknown <recruitkrishna2023@gmail.com>
2025-01-24 21:57:11 +0530

    commit_test

--- src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
+++ src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -1,5 +1,9 @@
 package net.engineeringdigest.journalApp.entity;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
@@ -7,42 +11,12 @@ import java.util.Date;
 
 @Entity
 @Table(name = "journal_entries")
+@Getter
+@Setter
 public class JournalEntry {
     @Id
     private String id;
     private String title;
     private String content;
     private Date date;
-
-    public Date getDate() {
-        return date;
-    }
-
-    public void setDate(Date date) {
-        this.date = date;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
